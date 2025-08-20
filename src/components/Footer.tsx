e81ac81b49505997e4e667c6c34a1fb0b72290ff
Its-Arup <aruppaul1650@gmail.com>
2025-08-20 23:26:45 +0530

    Update footer link to point to cnpgenerator.vercel.app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='w-full bg-[#202942] h-20'>
         <div className="mx-auto max-w-[1000px] w-full flex items-center text-gray-400 h-full">
-            <p>© 2025 CNP Generator - Site realizat de <Link className="text-[#0d6efd]" to={"/"}>deploy</Link></p>
+            <p>© 2025 CNP Generator - Site realizat de <Link className="text-[#0d6efd]" to={"/"}>cnpgenerator.vercel.app</Link></p>
         </div>
     </div>
   )
